a2c52762b6a0f43a9afaaa6f1e2bc32fd8736c53
Silver343 <51054165+Silver343@users.noreply.github.com>
2022-09-05 11:28:33 +0100

    menu resource

---
+++ resources/js/Pages/Restaurant/Show.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { Head, Link } from "@inertiaJs/inertia-vue3";
+
+defineProps({
+    restaurant: Object,
+})
+</script>
+
+<template>
+    <Head>
+        <title>{{ restaurant.data.name }}</title>
+    </Head>
+    <div>
+        {{ restaurant.data.name }}
+
+        <li v-for="menu in restaurant.data.menus" :key="menu.id">
+            {{ menu.name}}
+        </li>
+    </div>
+</template>
